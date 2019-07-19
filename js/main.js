e7dc1cd9a045e31bf515b6d22c484490335cbb96
Iryna-Nepranova <i.nepranova@gmail.com>
2019-07-19 15:40:20 +0300

    last version

--- js/main.js
+++ js/main.js
@@ -14,17 +14,43 @@ var swiper = new Swiper('.swiper-container', {
   });
 
   
-$('.my-class').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      // when window width is <= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-disabled',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      // when window width is <= 640px
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      }
+    }
   });
 
 })
 
-$(document).ready(function(){
-  $('.slider').bxSlider();
-
-});
